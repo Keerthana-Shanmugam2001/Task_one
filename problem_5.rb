38111aef12dd25b2b019208db880c788de775f6f
Keerthana-Shanmugam2001 <keerthana.shanmugam@freshworks.com>
2023-02-21 14:25:05 +0530

    modified changes

--- problem_5.rb
+++ problem_5.rb
@@ -1,7 +1,14 @@
 # Write a program that takes a string as input and returns the number of vowels in the string.
 
-inputs = puts "Enter some line of words: "
-inputs = gets.chomp
-vowels = "a", "e", "i", "o", "u"
-answer = inputs.find_by(vowels.count)
-puts answer
+string = puts "Enter some line of words: "
+string = gets.chomp
+num_vowels = 0
+i = 0
+while i < string.length
+    if string[i] == "a" || string[i] == "e" || string[i] == "i" || string[i] == "o" || string[i] == "u"
+        puts num_vowels += 1
+    end
+    i += 1
+end
+return num_vowels
+
--- problem_6.rb
+++ problem_6.rb
@@ -1,4 +1,5 @@
 # Write a program that reads a string and prints the reverse of the string.
 
-puts "hello world"
-puts "hello world".reverse
+result = puts "Enter some words: "
+puts result = gets.chomp.reverse
+
