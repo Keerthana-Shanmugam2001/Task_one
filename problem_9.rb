722eb562538c8e0cde67e81a542f8e5a3bf6c585
Keerthana-Shanmugam2001 <keerthana.shanmugam@freshworks.com>
2023-02-21 14:28:44 +0530

    modified changes

--- problem_9.rb
+++ problem_9.rb
@@ -1,18 +1,11 @@
 # Write a program that takes two lists as input and returns a list containing the common elements of the two input lists.
-list1 = [1, 3, 4, 5, 7, 9, 8]
+list1 = [1, 3, 4, 5, 7, 8, 9]
 list2 = [2, 4, 6, 8, 9, 10]
-result = false
+print result = false
 for x in list1
     for y in list2
         if x==y
-            result = true
-            print result
+            print result = true
         end
     end
 end
-
-
-# if list1 && list2
-#     result = arr.push
-# end
-# print result
